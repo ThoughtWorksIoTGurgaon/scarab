2de26409cfb41027677a1035d5472bc83f8c2133
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-08-20 16:26:17 +0530

    Adding few testcases for Packet::parseRead

--- test/Packet.cpp
+++ test/Packet.cpp
@@ -1,7 +1,40 @@
 #include "gtest/gtest.h"
 #include "Packet.h"
 
-TEST(packet, parseRead)
+TEST(packet, shouldReadPacketForOneServiceAndOnceCharacteristic)
 {
-    ASSERT_EQ(1, 1);
+  char binPkt[] = {
+    0x01, 0x03, 0x01, 0x01, 0x01, //header
+    0x01,  //serviceId
+    0x01, 0x01 //charCount, charIds
+  };
+
+  ReadPacket* readPacket = Packet :: parseRead(binPkt);
+  ASSERT_EQ(readPacket->serviceId, 0);
+  ASSERT_EQ(readPacket->charCount, 1);
+
+  char characteristicIds[] = {0x01};
+
+  for (size_t i = 0; i < readPacket->charCount; ++i) {
+    ASSERT_EQ(readPacket->characteristicIds[i], characteristicIds[i]);
+  }
+}
+
+TEST(packet, shouldReadPacketForOneServiceAndTwoCharacteristic)
+{
+  char binPkt[] = {
+    0x01, 0x03, 0x01, 0x01, 0x01, //header
+    0x04,  //serviceId
+    0x02, 0x02, 0x01 //charCount, charIds...
+  };
+
+  ReadPacket* readPacket = Packet :: parseRead(binPkt);
+  ASSERT_EQ(readPacket->serviceId, 3);
+  ASSERT_EQ(readPacket->charCount, 2);
+
+  char characteristicIds[] = {0x02, 0x01};
+
+  for (size_t i = 0; i < readPacket->charCount; ++i) {
+    ASSERT_EQ(readPacket->characteristicIds[i], characteristicIds[i]);
+  }
 }
