593a1ff775728ce6276b261aed275f2df898aeb8
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-08-20 17:06:18 +0530

    Adding few more testcases for Packet::readPacket.

--- test/Packet.cpp
+++ test/Packet.cpp
@@ -1,7 +1,20 @@
 #include "gtest/gtest.h"
 #include "Packet.h"
 
-TEST(packet, shouldReadPacketForOneServiceAndOnceCharacteristic)
+TEST(packet, shouldReadPacketForOneServiceAndZeroCharacteristic)
+{
+  char binPkt[] = {
+    0x01, 0x03, 0x01, 0x01, 0x01, //header
+    0x01,  //serviceId
+    0x00  //charCount
+  };
+
+  ReadPacket* readPacket = Packet :: parseRead(binPkt);
+  ASSERT_EQ(readPacket->serviceId, 0);
+  ASSERT_EQ(readPacket->charCount, 0);
+}
+
+TEST(packet, shouldReadPacketForOneServiceAndOneCharacteristic)
 {
   char binPkt[] = {
     0x01, 0x03, 0x01, 0x01, 0x01, //header
@@ -37,4 +50,26 @@ TEST(packet, shouldReadPacketForOneServiceAndTwoCharacteristic)
   for (size_t i = 0; i < readPacket->charCount; ++i) {
     ASSERT_EQ(readPacket->characteristicIds[i], characteristicIds[i]);
   }
+}
+
+TEST(packet, shouldReadPacketAndReturnWithoutConsumingAnyMemory)
+{
+  char binPkt1[] = {
+    0x01, 0x03, 0x01, 0x01, 0x01, //header
+    0x01,  //serviceId
+    0x00  //charCount
+  };
+
+  ReadPacket* readPacket1 = Packet :: parseRead(binPkt1);
+
+  char binPkt2[] = {
+    0x01, 0x03, 0x01, 0x01, 0x01, //header
+    0x04,  //serviceId
+    0x02, 0x02, 0x01 //charCount, charIds...
+  };
+
+  ReadPacket* readPacket2 = Packet :: parseRead(binPkt2);
+
+  //both readPackets are pointing to same memory address
+  ASSERT_EQ(readPacket1, readPacket2);
 }
