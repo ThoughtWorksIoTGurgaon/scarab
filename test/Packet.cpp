a3c5481a2dc81833c1478f49cfac02f019c62152
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-08-22 00:17:31 +0530

    Adding one more test case for Packet::stringifyResponse.

--- test/Packet.cpp
+++ test/Packet.cpp
@@ -92,3 +92,29 @@ TEST(packet, shouldStringifyResponseHavingZeroCharsData)
 
   ASSERT_STREQ(responseString, expectedResponseString);
 }
+
+TEST(packet, shouldStringifyResponseHavingOneCharsData)
+{
+  ResponsePacket *pkt = &Packet::responsePacket;
+
+  pkt->serviceId = 3;
+  pkt->charCount = 1;
+  pkt->charsStruct = new CharStruct[1];
+  pkt->charsStruct[0].id = 1;
+  pkt->charsStruct[0].dataLen = 1;
+  pkt->charsStruct[0].data = new char[1];
+  pkt->charsStruct[0].data[0] = 0x01;
+
+  char *responseString = Packet :: stringifyResponse(pkt);
+  char expectedResponseString[] = {
+    0x01, //Version
+    0x04, //ResponsePacketType
+    0x01, 0x01, 0x01, //Unused
+    0x04,  //serviceId
+    0x01, //charsCount
+    0x01, 0x01, 0x01, //firstChar Id, dataLen, data
+    0x00 //Ending string
+  };
+
+  ASSERT_STREQ(responseString, expectedResponseString);
+}
